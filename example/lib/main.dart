463fc911d9be1a61c91b2cde2d4bdc293b199cd9
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-08-25 10:30:46 +0530

    docs: update in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,27 +8,11 @@ void main() {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -55,6 +39,10 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
+          const SizedBox(
+            height: 16,
+          ),
+          const Text('Example : For a custom color segment control'),
           Center(
             child: CustomSegmentControl(
               option1Text: 'Free',
@@ -66,6 +54,49 @@ class _MyAppState extends State<MyApp> {
               },
               option1SelectedColor: Colors.black,
               option2SelectedColor: Colors.red,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text('Example : For a decoration image'),
+          Center(
+            child: CustomSegmentControl(
+              option1Text: 'Free',
+              option2Text: 'Pro',
+              onOptionChange: (isProMode) {
+                setState(() {
+                  _pro = isProMode;
+                });
+              },
+              option1SelectedColor: Colors.black,
+              option2DecorationImage: const DecorationImage(
+                image: AssetImage('assets/images/example1.jpg'),
+                fit: BoxFit.fitWidth,
+              ),
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Example : For custom inactive window color specification',
+          ),
+          Center(
+            child: CustomSegmentControl(
+              option1Text: 'Free',
+              option2Text: 'Pro',
+              onOptionChange: (isProMode) {
+                setState(() {
+                  _pro = isProMode;
+                });
+              },
+              backgroundColor: Colors.green,
+              option1SelectedColor: Colors.black,
               fontSize: 16,
               fontWeight: FontWeight.bold,
             ),
